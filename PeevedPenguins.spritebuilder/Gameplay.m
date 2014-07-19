342320176fdfff7fff2e97ca309b3350cf5d8cda
Parker <Pavlicek@Parkers-Mac-mini.local>
2014-07-18 20:55:28 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -120,7 +120,7 @@
     [_contentNode runAction:follow];
 }
 
--(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNODE *)nodeB{
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB{
     CCLOG(@"Something collided with a seal!");
 }
 
